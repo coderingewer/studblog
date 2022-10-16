2e5ddb31ca716d2d9fd4a9e736a1bd90eb25e15d
Your Name <you@example.com>
2022-10-16 11:21:40 +0300

    form

--- src/Post/BeBlogger.jsx
+++ src/Post/BeBlogger.jsx
@@ -5,7 +5,7 @@ function BeBlogger() {
     return (
         <div className='be-blogger-page'>
             <p>Yazar Değil misiniz?</p>
-           <a className='link'   target="_blank"  href='https://forms.gle/dcEPkf6FU5YYsxsm9'> Yazar olun</a>
+           <a className='link'   target="_blank"  href='https://docs.google.com/forms/d/e/1FAIpQLSfatdf6i6n7v5_kWcOtG2_3taDRwqW-8oweU-SX3OQ9X7aNTw/viewform'> Yazar olun</a>
         </div>
     )
 }
